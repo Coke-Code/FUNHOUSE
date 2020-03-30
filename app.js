6d4a9c2b72f412fd05eac7ee0a2cc6d98f604bb6
lianghui <liangh@imyfone.cn>
2020-03-30 12:20:36 +0800

    2020年3月30日12:20:28 梁辉 更新服务器代码

--- app.js
+++ app.js
@@ -1,46 +1,49 @@
-var createError = require('http-errors');
+process.env.TMPDIR = 'tmp'; // to avoid the EXDEV rename error, see http://stackoverflow.com/q/21071303/76173
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var crossdomain = require('./code/crossdomin')
-var uploader = require('./code/upload')
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+var uploader = require('./code/uploader-node.js')('tmp');
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-//设置允许跨域访问该服务.
-app.all('*', crossdomain.setting);
-
-app.post('/upload', uploader.multerset.single('file'), uploader.singlefile)
-
-app.post('/uploads', uploader.multerset.array('file', 10), uploader.miltifile)
-
-app.use(uploader.catcherror)
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Configure access control allow origin header stuff
+var ACCESS_CONTROLL_ALLOW_ORIGIN = true;
+// Host most stuff in the public folder
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/../../dist'));
+// Handle uploads through Uploader.js
+app.post('/upload', multipartMiddleware, function(req, res) {
+  uploader.post(req, function(status, filename, original_filename, identifier) {
+    console.log('POST', status, original_filename, identifier);
+    if (ACCESS_CONTROLL_ALLOW_ORIGIN) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "content-type")
+    }
+    setTimeout(function () {
+      res.send(status);
+    }, 500);
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.options('/upload', function(req, res){
+  console.log('OPTIONS');
+  if (ACCESS_CONTROLL_ALLOW_ORIGIN) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "content-type")
+  }
+  res.status(200).send();
 });
 
-module.exports = app;
+// Handle status checks on chunks through Uploader.js
+app.get('/upload', function(req, res) {
+  uploader.get(req, function(status, filename, original_filename, identifier) {
+    console.log('GET', status);
+    if (ACCESS_CONTROLL_ALLOW_ORIGIN) {
+      res.header("Access-Control-Allow-Origin", "*");
+    }
+    res.status(status == 'found' ? 200 : 204).send(status);
+  });
+});
+app.get('/download/:identifier', function(req, res) {
+  uploader.write(req.params.identifier, res);
+});
+
+app.listen(3000);
